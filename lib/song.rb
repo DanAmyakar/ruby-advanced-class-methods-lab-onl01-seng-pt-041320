f6b6511675b2b9650a8a3eb111cf7f7e7415d216
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-28 23:34:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,18 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    @@all << self
+  end
   
+  def self.new_by_name(name)
+    @@all << self(:name(name))
+  end
+  
+  def self.create_by_name
+  end
+  
+  def self.find_by_name
+  end
 
 end
