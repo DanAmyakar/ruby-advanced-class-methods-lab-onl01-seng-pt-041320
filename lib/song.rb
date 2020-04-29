812f507643bad63b468b3e63ebfbe0b85a420907
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-29 00:54:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,8 @@ class Song
   def self.find_or_create_by_name(name)
     #if self.find_or_create_by_name(name) == nil
     # self.create_by_name(name)
-    
+    #else
+    # 
 
   def self.alphabetical()
     @@all.sort_by{|a|a.name}
